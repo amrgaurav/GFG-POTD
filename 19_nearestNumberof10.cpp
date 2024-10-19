27db499f2bab54c75b84230e32dba6fb2005baa8
Gaurav Kumar <121360717+amrgaurav@users.noreply.github.com>
2024-10-19 19:11:20 +0530

    Create 19_nearestNumberof10.cpp

---
+++ 19_nearestNumberof10.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    pair<long long, long long> getMinMax(vector<long long int> arr) {
+        // code here
+        int mini=INT_MAX,maxi=INT_MIN;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(mini>arr[i])
+            {
+                mini=arr[i];
+            }
+            if(maxi<arr[i])
+            {
+                maxi=arr[i];
+            }
+        }
+        return {mini,maxi};
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<long long int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(arr);
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
